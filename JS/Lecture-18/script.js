d29c604135e924d622a90f1e318eefa42030bb0d
santosh <santoshtajpur@gmail.com>
2024-01-10 17:41:24 +0530

    Project[Calculator,simonGame]

---
+++ JS/Lecture-18/script.js
@@ -0,0 +1,83 @@
+// console.log('OOPs');
+
+
+/*
+
+class
+object
+prototype
+prototype channing
+polyfill
+inheritance
+extends
+super
+encapsulation
+polymorphism
+
+
+
+*/
+
+// let arr=[];
+// Array.prototype.apna=function(){
+//     console.log('Apna');
+// }
+
+// let obj={};
+
+// function add(){
+    
+// }
+// // console.log(add);
+
+// console.log(add);
+// console.log(arr);
+// console.log(obj);
+
+//session storage
+
+// let name='santosh';
+//  sessionStorage.setItem('newName',name);
+//  let arr=[1,2,3,4,5];
+//  let obj={
+//     fname:'Santosh',
+//     lname:'Kumar'
+//  }
+//  sessionStorage.setItem('newObj',JSON.stringify(obj));
+//  sessionStorage.setItem('newArr',JSON.stringify(arr));
+//  console.log(JSON.parse(sessionStorage.getItem('newObj')));
+//  console.log(JSON.parse(sessionStorage.getItem('newArr')));
+
+
+// class Employee {
+//      Name(fname,lnmae) {
+//         this.fname=fname;
+//         this.lnmae=lnmae;
+        
+//     };
+
+//     PrintFullName(){
+//         document.write(`${this.fname} ${this.lnmae} `);
+//     }
+// }
+
+// let e1= new Employee('Santosh','Kumar');
+// let e2= new Employee('Pawan','Sharma');
+// let e3= new Employee('Anil','Kumar');
+// let e4= new Employee('Sunil','Kumar');
+// e1.PrintFullName();
+// e2.PrintFullName();
+// e3.PrintFullName();
+// e4.PrintFullName();
+
+//Polyfill
+
+let arr=[];
+
+Array.prototype.vishwajit=function(){
+    console.log('Vishawajit');
+}
+// arr.vishwajit();
+// console.log(arr);
+
+// console.log(arr.vishwajit());
