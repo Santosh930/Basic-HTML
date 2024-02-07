86384be89b8c9d2a281bfc567af5760bdcd48062
santosh <santoshtajpur@gmail.com>
2024-02-07 17:28:05 +0530

    lecture-3

---
+++ NodeJs/Lecture-3/wrapper.js
@@ -0,0 +1,14 @@
+// (function(exports,module,require,__dirname,__filename){
+//     const name='Santosh Kumar';
+//     console.log(name);
+
+// })();
+
+//private data
+
+// IIFE (Immediately Invoked Function Expression)
+// An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined. 
+
+// (function(a,b){
+//     console.log(a+b);
+// })(20,10);
